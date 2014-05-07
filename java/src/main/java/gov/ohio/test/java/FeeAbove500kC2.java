09708372c3294e85ef11237d75cd5a20bc30983b
marqJava <marq.java@gmail.com>
2014-05-07 01:29:18 -0500

    Code for Use Case 2

---
+++ java/src/main/java/gov/ohio/test/java/FeeAbove500kC2.java
@@ -0,0 +1,28 @@
+package gov.ohio.test.java;
+
+import java.math.BigDecimal;
+
+public class FeeAbove500kC2 extends Fee {
+
+	public FeeAbove500kC2() {
+		super(new BigDecimal(500000), new BigDecimal(999999999), 0.05, BigDecimal.ZERO);
+	}
+
+	@Override
+	public BigDecimal calculate() {
+		BigDecimal res = new BigDecimal(0);
+		BigDecimal multiplicand = new BigDecimal(this.feePercentage);
+		res.setScale(2);
+
+		if ((this.amount.intValue() > 0 && this.topLimit.compareTo(this.lowLimit) == 1)) {
+
+			if (this.amount.compareTo(this.lowLimit) == 1) {
+				res = this.amount.subtract(this.lowLimit);
+				res = res.multiply(multiplicand);
+			}
+		}
+
+		return res;
+	}
+
+}
